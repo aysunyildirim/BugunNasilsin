ac6917b7e06433887841cd2ef6a5056cc462e672
Aysun YILDIRIM <82328345+aysunyildirim@users.noreply.github.com>
2023-05-28 15:40:29 +0300

    Add project files.

---
+++ BugunNasilsin/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+namespace Program
+{ 
+class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba !");
+    Console.WriteLine("Bugun Nasilsin?");
+    var hal = Console.ReadLine();
+    Console.WriteLine();
+    Console.WriteLine("Ben de" + hal);
+    Console.WriteLine(hal);
+    Console.ReadLine();
+
+    }
+}
+}
